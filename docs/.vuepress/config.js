c131b480a408b976657690415762f143689ce9ee
jiabinxu <13804970786@163.com>
2025-04-12 19:18:37 +0800

    更新blog相关，增加JavaScript部分

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,14 @@
-const path = require("path"); // 新增这行
+const path = require("path");
 const fs = require("fs");
+
 module.exports = {
   theme: "@vuepress/theme-default",
   description: "前端开发知识体系",
   base: "/jiabinxu-blog/",
 
-  // 多语言配置
+  // 多语言配置（保持原样）
   locales: {
-    "/zh/": {
+    "/": {
       lang: "zh-CN",
       title: "贾滨旭的个人技术博客",
       description: "前端开发知识体系",
@@ -20,32 +21,44 @@ module.exports = {
   },
   themeConfig: {
     locales: {
-      "/zh/": {
+      "/": {
         nav: [
-          { text: "Home", link: "/zh/" },
-          { text: "en", link: "/en/" },
+          { text: "Home", link: "/" },
+          { text: "Css", link: "/css/" },
+          { text: "JavaScript", link: "/JavaScript/" },
           { text: "External", link: "https://google.com" },
         ],
+        // sidebar: generateSidebar(),
         sidebar: {
-          "/zh/html/": [
+          "/html/": [
             { title: "advanced", path: "advanced" },
             { title: "basics", path: "basics" },
+            { title: "面试相关", path: "面试" },
           ],
-          "/zh/css/": ["layout"],
+          "/css/": [{ title: "layout", path: "layout" }],
+          "/JavaScript/": [{ title: "常见面试手写题", path: "常见面试手写题" }],
         },
       },
       "/en/": {
         nav: [
           { text: "Home", link: "/en/" },
-          { text: "zh", link: "/zh/" },
+          { text: "Css", link: "/en//css/" },
           { text: "External", link: "https://google.com" },
         ],
+        // sidebar: generateSidebar(),
         sidebar: {
           "/en/html/": [
             { title: "advanced", path: "advanced" },
             { title: "basics", path: "basics" },
+            { title: "interview", path: "interview" },
           ],
-          "/en/css/": ["layout"],
+          "/en/css/": [{ title: "layout", path: "layout" }],
+          "/en/JavaScript/": [
+            {
+              title: "Common-written-test-questions",
+              path: "Common-written-test-questions",
+            },
+          ],
         },
       },
     },
